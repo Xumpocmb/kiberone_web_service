bbfdd2dcb77c34f1fe366bd1d50803e012f0239f
xumpocmb <x.xumpocmb@gmail.com>
2025-04-10 20:41:51 +0300

    api endpoints

--- app_api/urls.py
+++ app_api/urls.py
@@ -15,18 +15,11 @@ urlpatterns = [
     path("find_user_in_crm/", find_user_by_phone_view, name="find_user"),
     path("find_user_in_db/", find_user_in_db_view, name="find_user_in_db"),
     path("register_user_in_db/", register_user_in_db_view, name="register_user_in_db"),
-    path(
-        "register_user_in_crm/", register_user_in_crm_view, name="register_user_in_crm"
-    ),
-    path(
-        "create_or_update_clients_in_db/",
-        create_or_update_clients_in_db_view,
-        name="create_or_update_clients_in_db",
-    ),
+    path("register_user_in_crm/", register_user_in_crm_view, name="register_user_in_crm"),
+    path("create_or_update_clients_in_db/", create_or_update_clients_in_db_view, name="create_or_update_clients_in_db"),
+    
     path("questions/", get_all_questions, name="questions"),
-    path(
-        "answer_by_question/<int:question_id>/",
-        get_answer_by_question_id,
-        name="answer_by_question",
-    ),
+    path("answer_by_question/<int:question_id>/", get_answer_by_question_id, name="answer_by_question"),
+    
+    
 ]
