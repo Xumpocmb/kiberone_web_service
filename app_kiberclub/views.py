ef1fe34482d20aef6fdcb21e9825b8d5ce3b91e5
xumpocmb <x.xumpocmb@gmail.com>
2025-04-29 11:25:15 +0300

    index view

--- app_kiberclub/views.py
+++ app_kiberclub/views.py
@@ -4,6 +4,6 @@ def index(request):
     context = {
         "title": "KIBERone",
     }
-    telegram_id = request.GET.get('telegram_id')
+    telegram_id = request.GET.get('user_tg_id')
     context.update({"tg_id": telegram_id})
     return render(request, 'app_kiberclub/greetings.html', context=context)
