fb716f3e5fbc84e1e4a269382b0ac5368a7cf353
xumpocmb <x.xumpocmb@gmail.com>
2025-04-29 11:52:59 +0300

    index view

--- app_kiberclub/views.py
+++ app_kiberclub/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 
+from app_kiberclub.models import AppUser, Client
+
+
 def index(request):
     context = {
         "title": "KIBERone",
     }
-    telegram_id = request.GET.get('user_tg_id')
-    context.update({"tg_id": telegram_id})
+    telegram_id_from_req: str = request.GET.get('user_tg_id')
+
+    bot_user = AppUser.objects.get(telegram_id=telegram_id_from_req)
+    user_clients = Client.objects.filter(user=bot_user)
+    context.update({"profiles": user_clients})
     return render(request, 'app_kiberclub/greetings.html', context=context)
