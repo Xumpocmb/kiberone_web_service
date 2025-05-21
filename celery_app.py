481836acf2e9bad07ec4cb39ee8384b0782db083
xumpocmb <x.xumpocmb@gmail.com>
2025-05-21 20:13:24 +0300

    fix client name in admin panel

--- celery_app.py
+++ celery_app.py
@@ -1,8 +1,6 @@
-from celery import Celery
-from django.conf import settings
 import os
 
-from celery.schedules import crontab
+from celery import Celery
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "_web_service.settings")
 
@@ -11,5 +9,4 @@ app = Celery("_web_service")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
 
-# Используем планировщик из базы данных
 app.conf.beat_scheduler = "django_celery_beat.schedulers:DatabaseScheduler"
